cf61e54f7224d8b3d8e5f5ff2a038ca6212679be
RufatGulabli <gulabli.rufat@gmail.com>
2019-09-25 19:58:35 +0400

    sdadasdsad

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,8 @@ const moviesController = require('../controllers/movies');
 const genresController = require('../controllers/genres');
 const customersController = require('../controllers/customers');
 const rentalsController = require('../controllers/rentals');
+const registerController = require('../controllers/register');
+const authController = require('../controllers/login');
 const { expressErrorHandler, pageNotFoundErrorHandler } = require('../middlewares/error-handler');
 
 module.exports = app => {
@@ -11,10 +13,12 @@ module.exports = app => {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(cors());
     app.options('*', cors());
-    app.use('/movies', moviesController);
-    app.use('/genres', genresController);
-    app.use('/customers', customersController);
-    app.use('/rentals', rentalsController);
+    app.use('/api/movies', moviesController);
+    app.use('/api/genres', genresController);
+    app.use('/api/customers', customersController);
+    app.use('/api/rentals', rentalsController);
+    app.use('/api/register', registerController);
+    app.use('/api/auth', authController);
     app.use(expressErrorHandler);
     app.use(pageNotFoundErrorHandler);
 }
